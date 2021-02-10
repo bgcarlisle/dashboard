021a5f7ef9d13646ce1fdba6c209a9f6e4e91add
Benjamin G Carlisle <bgcarlisle@users.noreply.github.com>
2021-02-10 23:35:49 +0100

    Moved start page to uiOutput to allow dynamic drop-down generation

--- shiny_app/app.R
+++ shiny_app/app.R
@@ -137,6 +137,62 @@ server <- function (input, output, session) {
         }
     )
 
+    ## Dynamically generate options for UMC drop-down menu
+
+    output$startpage <- renderUI({
+
+        wellPanel(
+            br(),
+            fluidRow(
+                column(
+                    8,
+                    h1(style = "margin-left:0cm", strong("Responsible Metrics Dashboard"), align = "left"),
+                    h4(style = "margin-left:0cm",
+                       "This dashboard is a proof-of-principle overview of several metrics of open and responsible research for several German University Medical Centres (UMC's). For more detailed information on the methods used to calculate those metrics, the dataset underlying the metrics, or resources to improve your own research practices, click one of the following buttons."),
+                    h4(style = "margin-left:0cm",
+                       "This dashboard is a pilot that is still under development, and should not be used to compare UMC's or inform policy. More metrics may be added in the future."),
+                    br()
+                ),
+                column(
+                    4,
+                    hr(),
+                    br(),
+                    br(),
+                    actionButton(
+                        style = "color: white; background-color: #aa1c7d;",
+                        'buttonAllUMCs',
+                        'See all UMC\'s'
+                    ),
+                    actionButton(
+                        style = "color: white; background-color: #aa1c7d;",
+                        'buttonMethods',
+                        'See methods'
+                    ),
+                    actionButton(
+                        style = "color: white; background-color: #aa1c7d;",
+                        'buttonDatasets',
+                        'See data sets'
+                    ),
+                    br()
+                )
+            ),
+            fluidRow(
+                column(
+                    4,
+                    br(),
+                    br(),
+                    selectInput(
+                        "selectUMC",
+                        strong("Choose UMC"),
+                        choices = umclist,
+                        selected = NA
+                    )
+                )
+            )
+        )
+        
+    })
+
     ## Dynamically determine column width for displayed metrics
     ## at program start; four columns if resolution large enough,
     ## otherwise two columns.
