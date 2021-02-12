bc2e782058bf396e1177672189949af7c97b4527
Delwen Franzen <delwen@franzen.fm>
2021-02-12 12:01:54 +0100

    Add tool tips
    - IACUC tool tip is commented out until we decide whether to include it

--- shiny_app/methods_page.R
+++ shiny_app/methods_page.R
@@ -322,28 +322,108 @@ methods_page <- tabPanel(
 
 ## Tooltips for Open Science metrics
 
-openaccess_tooltip <- strwrap("This metric ...")
+openaccess_tooltip <- strwrap("The Open Access metric shows the percentage of research publications that are
+                             published as Open Access (OA) articles. Gold OA denotes publication in a pure
+                             OA journal. Green OA denotes a freely available repository version. Hybrid OA
+                             denotes an OA publication in a journal with offers both a subscription based
+                             model as well as an Open Access option. For some articles no Open Access
+                             information was available.") %>%
 
-opendata_tooltip <- strwrap("This metric ...")
+paste(collapse = " ")
 
-opencode_tooltip <- strwrap("This metric ...")
+opendata_tooltip <- strwrap("The Open Data metric measures the percentage of publications in English and for
+                            which the full text could be screened that state that they shared their research data.
+                            Openly shared data makes research more transparent, as research findings can be
+                            reproduced. Additionally, shared datasets can be reused and combined by other
+                            scientists to answer new research questions.") %>%
+
+paste(collapse = " ")
+
+opencode_tooltip <- strwrap("The Open Code metric measures the percentage of publications in English and for
+                            which the full text could be screened that state that they shared their analysis code.
+                            Like openly shared data, Open Code makes research more transparent, as research
+                            findings can be reproduced.") %>%
+
+paste(collapse = " ")
 
 ## Tooltips for Clinical Trials metrics
 
-trn_tooltip <- strwrap("This metric ...")
+trn_tooltip <- strwrap("This metric measures how many publications classified by PubMed as clinical trial
+                        publications report a trial registration number (TRN) in the abstract and in the
+                        secondary identifier metadata of the publication. Reporting of clinical trial registration
+                        numbers in related publications facilitates transparent linkage between registration and
+                        publication. The CONSORT as well as the ICMJE Recommendations for the Conduct, Reporting,
+                        Editing, and Publication of Scholarly Work in Medical Journals call for reporting
+                        <i>&#39trial registration number and name of the trial register&#39</i> in both the
+                       full-text and abstract.") %>%
 
-sumres_tooltip <- strwrap("This metric ...")
+paste(collapse = " ")
 
-prereg_tooltip <- strwrap("This metric ...")
+sumres_tooltip <- strwrap("This metric measures how many clinical trials registered in the
+                        EU Clinical Trials Register that are due to report their results have already
+                        done so. A trial is due to report its results 12 month after trial completion.
+                        The data were retrieved from the EU Trials Tracker by the EBM DataLab
+                        (eu.trialstracker.net). Clinical trials are expensive and have often many contributing
+                        patients. A fast dissemination of the trial results is crucial to make the evidence
+                        gained in those trials available. The World Health organization recommends publishing
+                        clinical trial results within one year after the end of a study.") %>%
+    
+paste(collapse = " ")
 
-timpub_tooltip <- strwrap("This metric ...")
+prereg_tooltip <- strwrap("This metric measures if the clinical trials are registered before the
+                        start date of the study, according to the information given on ClinicalTrials.gov.
+                        The idea of prospective registration of studies is to make the trial specifications,
+                        including primary and secondary outcomes, publicly available before study start.
+                        Prospective registration adds transparency, helps protect against outcome switching.") %>%
+    
+paste(collapse = " ")
+
+timpub_tooltip <- strwrap("This metric measures how many clinical trials registered on ClinicalTrials.gov
+                        reported their results either as a journal publication or as summary results on the
+                        trials registry within 2 or 5 years after completion. Trials completed between 2009
+                        and 2013 were considered. The results were previously published as part of the
+                        IntoValue study (https://s-quest.bihealth.org/intovalue/). Clinical trials are expensive
+                        and often have many contributing patients. A fast dissemination of the trial results
+                        is crucial to make the evidence gained in those trials available. The World Health
+                        organization recommends publishing clinical trial results within one year after the
+                        end of a study.") %>%
+
+paste(collapse = " ")
 
 ## Tooltips for Robustness metrics
 
-randomization_tooltip <- strwrap("This metric shows ...")
+randomization_tooltip <- strwrap("This metric measures how many animal studies report a statement on
+                            randomization of subjects into groups. Animal studies were identified using a
+                            previously published PubMed search filter. Reporting of randomization was evaluated
+                            with SciScore, an automated tool which evaluates research articles based on their
+                            adherence to rigour and reproducibility criteria. Only publications in the
+                            PubMed Central corpus and which could be analyzed by SciScore are were included
+                            in this analysis.") %>%
+    
+paste(collapse = " ")
 
-blinding_tooltip <- strwrap("This other metric shows ...")
 
-power_tooltip <- strwrap("This other metric shows ...")
+blinding_tooltip <- strwrap("This metric measures how many animal studies report a statement on whether
+                            investigators were blinded to group assignment and/or outcome assessment. Animal
+                            studies were identified using a previously published PubMed search filter. Reporting
+                            of blinding was evaluated with SciScore, an automated tool which evaluates research
+                            articles based on their adherence to rigour and reproducibility criteria. Only
+                            publications in the PubMed Central corpus and which could be analyzed by SciScore
+                            are were included in this analysis.") %>%
 
-iacuc_tooltip <- strwrap("This other metric shows ...")
+paste(collapse = " ")
+
+
+power_tooltip <- strwrap("This metric measures how many animal studies report a statement on sample size
+                         calculation. Animal studies were identified using a previously published PubMed search
+                         filter. Reporting of sample size calculation was evaluated with SciScore, an automated
+                         tool which evaluates research articles based on their adherence to rigour and
+                         reproducibility criteria. Only publications in the PubMed Central corpus and which
+                         could be analyzed by SciScore are were included in this analysis.") %>%
+    
+paste(collapse = " ")
+
+# iacuc_tooltip <- strwrap("This metric measures how many animal studies report an Institutional animal care and
+#                          use committee statement.") %>%
+#     
+# paste(collapse = " ")
