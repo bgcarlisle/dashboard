59dbf0de965f621f7beeaa9ddfaad9913b76b9da
Delwen Franzen <delwen@franzen.fm>
2021-02-12 16:36:13 +0100

    Add publication year to TRN plot description

--- shiny_app/app.R
+++ shiny_app/app.R
@@ -547,7 +547,7 @@ server <- function (input, output, session) {
                     metric_box(
                         title = "Trial Registry Number Reporting",
                         value = paste0(round(100*all_numer_trn/all_denom_trn), "%"),
-                        value_text = "of PubMed-classified clinical trial publications reported a TRN in the abstract",
+                        value_text = "of 2018 PubMed-classified clinical trial publications reported a TRN in the abstract",
                         plot = plotlyOutput('plot_clinicaltrials_trn', height="300px"),
                         info_id = "infoTRN",
                         info_title = "Trial Registry Number Reporting",
@@ -926,7 +926,7 @@ server <- function (input, output, session) {
                     metric_box(
                         title = "Trial Registry Number Reporting",
                         value = paste0(round(100*all_numer_trn/all_denom_trn), "%"),
-                        value_text = "of clinical trial publications reported a TRN in the abstract",
+                        value_text = "of 2018 PubMed-classified clinical trial publications reported a TRN in the abstract",
                         plot = plotlyOutput('plot_allumc_clinicaltrials_trn', height="300px"),
                         info_id = "infoALLUMCTRN",
                         info_title = "TRN reporting (All UMCs)",
