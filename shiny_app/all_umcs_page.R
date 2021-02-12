edcc0e58c96143b1ad7c640f2bf14152d61ee49d
Delwen Franzen <delwen@franzen.fm>
2021-02-12 16:11:31 +0100

    Minor fixes to description of all UMC page
    - Changed UMC's to UMCs: sorry this was bugging me
    - Specified this is a proof-of-principle dashboard (align with start page)
    - Removed mention of datasets and tools to improve (align with start page)

--- shiny_app/all_umcs_page.R
+++ shiny_app/all_umcs_page.R
@@ -22,7 +22,7 @@ allumc_animal_iacuc_tooltip <- strwrap("This metric ...")
 
 ## Define the page layout
 all_umcs_page <- tabPanel(
-    "All UMC's", value = "tabAllUMCs",
+    "All UMCs", value = "tabAllUMCs",
     wellPanel(
         br(),
         fluidRow(
@@ -30,15 +30,15 @@ all_umcs_page <- tabPanel(
                 12,
                 h1(
                     style = "margin-left: 0",
-                    strong("Responsible Metrics Dashboard: All UMC's"),
+                    strong("Proof-of-principle Responsible Metrics Dashboard: All UMCs"),
                     align = "left"
                 ),
                 h4(
                     style = "margin-left: 0",
-                    "This dashboard provides an overview of the relative performance of several German University Medical Centres (UMC's) on several metrics of open and responsible research. For more detailed information on the methods used to calculate those metrics, the dataset underlying the metrics, or resources to improve your own research practices, click one of the following buttons."
+                    "This dashboard provides an overview of the relative performance of several German University Medical Centres (UMCs) on several metrics of open and responsible research. For more detailed information on the methods used to calculate those metrics, click one of the following buttons."
                 ),
                 h4(style = "margin-left:0cm",
-                   "This dashboard is a pilot that is still under development, and should not be used to compare UMC's or inform policy. More metrics may be added in the future."),
+                   "This dashboard is a pilot that is still under development, and should not be used to compare UMCs or inform policy. More metrics may be added in the future."),
                 br()
             )
         )
